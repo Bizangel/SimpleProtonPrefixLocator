67b3895b5ef2cbe3d52d763235c61a7e16ed27b5
Luis De Avila <bizangel10@gmail.com>
2025-09-14 22:02:52 +0100

    Added out folder for custom build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
 cd "$(dirname "$0")"
 cd ..
 NO_STRIP=true deno task tauri build
+rm -rf out/
+mkdir -p out/
+cp src-tauri/target/release/simple-proton-save-locator out/
+cp src-tauri/target/release/bundle/appimage/simple-proton-save-locator.AppDir/simple-proton-save-locator.png out/
+cp src-tauri/target/release/bundle/appimage/simple-proton-save-locator.AppDir/simple-proton-save-locator.desktop out/
